44e88dddd6a4be2e2ffc1eec39efc9fa69825670
marcoantonio40 <marcoudi40@gmail.com>
2019-06-28 19:03:35 -0300

    Gridview

--- Tela_Exibe_Notas.py
+++ Tela_Exibe_Notas.py
@@ -4,6 +4,20 @@ def Tela_Exibe_Notas():
 	janela = Tk()
 	janela.geometry("675x400+400+400")
 	
+
+	lbl1= Label(janela, text="Código Nota", bg="blue", fg="white")
+	lbl2= Label(janela, text="Usuário", bg="blue", fg="white")
+	lbl3= Label(janela, text="Título", bg="blue", fg="white")
+	lbl4= Label(janela, text="Descrição", bg="blue", fg="white")
+	lbl5= Label(janela, text="Data", bg="blue", fg="white")
+
+	lbl1.grid(row=0,column=2,padx= 10, pady=10)
+	lbl2.grid(row=0,column=3,padx= 10, pady=10)
+	lbl3.grid(row=0,column=4,padx= 10, pady=10)
+	lbl4.grid(row=0,column=5,padx= 10, pady=10)
+	lbl5.grid(row=0,column=6,padx= 10, pady=10)
+
+	
 	botao_Cadastrar_Nota = Button(janela, width=20,text="Cadastrar Nota", command = print("Cadastrar nota"))
 	botao_Cadastrar_Nota.place(x=20, y=370)
 		
