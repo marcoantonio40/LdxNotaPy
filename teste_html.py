02bf6373172b35ad739127ee36edbff2d87b3dc7
marcoantonio40 <marcoudi40@gmail.com>
2019-07-03 21:22:52 -0300

    html

--- teste_html.py
+++ teste_html.py
@@ -1,11 +1,30 @@
-#!/usr/bin/python
-import cgi
-
-# obtem referencia as variaveis passadas pelo cliente
-fields = cgi.FieldStorage()
-
-# cabecalho que informa o browser para renderizar como HTML
-print("Content-Type: text/html\n\n")
-
-nome = fields.getvalue('nome')
-print("<strong>Hello, %s!</strong>" % (nome))
+from IPython.display import HTML
+summary_table_2 = '''<table class="table table-striped">
+<th>Ticker</th><th>Full name</th>
+<tr>
+    <td>AAPL</td>
+    <td><a href="http://finance.yahoo.com/q?s=AAPL">Apple Inc</a></td>
+</tr>
+<tr>
+    <td>GE</td>
+    <td><a href="http://finance.yahoo.com/q?s=GE">General Electric Company</a></td>
+</tr>
+<tr>
+    <td>IBM</td>
+    <td><a href="http://finance.yahoo.com/q?s=IBM">International Business Machines Corp.</a></td>
+</tr>
+<tr>
+    <td>KO</td>
+    <td><a href="http://finance.yahoo.com/q?s=KO">The Coca-Cola Company</a></td>
+</tr>
+<tr>
+    <td>MSFT</td>
+    <td><a href="http://finance.yahoo.com/q?s=MSFT">Microsoft Corporation</a></td>
+</tr>
+<tr>
+    <td>PEP</td>
+    <td><a href="http://finance.yahoo.com/q?s=PEP">Pepsico, Inc.</a></td>
+</tr>
+</table>
+'''
+HTML(summary_table_2)
