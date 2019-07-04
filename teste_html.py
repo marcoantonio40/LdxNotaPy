706190bbde71deba0a34561b79b8306ba6f33566
marcoantonio40 <marcoudi40@gmail.com>
2019-07-04 09:28:43 -0300

    GridView

--- teste_html.py
+++ teste_html.py
@@ -1,30 +1,7 @@
-from IPython.display import HTML
-summary_table_2 = '''<table class="table table-striped">
-<th>Ticker</th><th>Full name</th>
-<tr>
-    <td>AAPL</td>
-    <td><a href="http://finance.yahoo.com/q?s=AAPL">Apple Inc</a></td>
-</tr>
-<tr>
-    <td>GE</td>
-    <td><a href="http://finance.yahoo.com/q?s=GE">General Electric Company</a></td>
-</tr>
-<tr>
-    <td>IBM</td>
-    <td><a href="http://finance.yahoo.com/q?s=IBM">International Business Machines Corp.</a></td>
-</tr>
-<tr>
-    <td>KO</td>
-    <td><a href="http://finance.yahoo.com/q?s=KO">The Coca-Cola Company</a></td>
-</tr>
-<tr>
-    <td>MSFT</td>
-    <td><a href="http://finance.yahoo.com/q?s=MSFT">Microsoft Corporation</a></td>
-</tr>
-<tr>
-    <td>PEP</td>
-    <td><a href="http://finance.yahoo.com/q?s=PEP">Pepsico, Inc.</a></td>
-</tr>
-</table>
-'''
-HTML(summary_table_2)
+from tkinter import Frame, Label, Message, StringVar
+from tkinter.constants import *
+
+def __init__():
+    print("oi")
+
+__init__()
